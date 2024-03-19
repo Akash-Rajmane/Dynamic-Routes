75648bf2fda9fb04a29f058f9791aa02173ebab1
Akash <akashrajmane23@gmail.com>
2024-03-19 12:55:20 +0530

    added delete function

--- models/product.js
+++ models/product.js
@@ -47,6 +47,21 @@ module.exports = class Product {
     });
   }
 
+  static deleteById(id){
+    getProductsFromFile(products => {
+      console.log(id);
+      const updatedProducts = products.filter(p => p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if(!err){
+          console.log("Product removed successfully");
+        } else {
+          console.error("Error removing product:", err);
+        }
+      });
+    });
+  }
+  
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
